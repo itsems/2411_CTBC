0193ba4d929693f09547448eeea692cefd8357ae
Emma <emma801006@hotmail.com>
2024-11-28 00:53:54 +0800

    chore: adjust Etf detail page structure for bg-gradient 1123ppt p.10

--- src/views/Etf/Detail/index.vue
+++ src/views/Etf/Detail/index.vue
@@ -1,46 +1,51 @@
 <template>
-  <GradientBackground :mobile-position="405" :desktop-position="410" />
-  <div class="relative pt-4 page-layout">
-    <Breadcrumb :list="breadCrumbList" class="!mb-4" />
-    <RouterLink :to="{ name: ROUTE_NAME.etf.list.name }">
-      <RoundButton class="!px-4 lg:!px-6 mb-2 ml-auto lg:absolute lg:right-0 lg:top-7" >
-        切換 ETF
-        <img
-          width="9"
-          class="ml-3"
-          src="@/assets/images/icon/arrow-r2.png"
-          alt="切換ETF"
-        />
-      </RoundButton>
-    </RouterLink>
-    <div class="inline-block px-3 py-1 mb-3 text-sm font-medium bg-white border border-solid rounded-2xl border-main text-main">海外股票型</div>
+  <div>
+    <div class="pt-4 pb-0 page-layout">
+      <Breadcrumb :list="breadCrumbList" class="!mb-4" />
+      <RouterLink :to="{ name: ROUTE_NAME.etf.list.name }">
+        <RoundButton class="!px-4 lg:!px-6 mb-2 ml-auto lg:absolute lg:right-0 lg:top-7" >
+          切換 ETF
+          <img
+            width="9"
+            class="ml-3"
+            src="@/assets/images/icon/arrow-r2.png"
+            alt="切換ETF"
+          />
+        </RoundButton>
+      </RouterLink>
+      <div class="inline-block px-3 py-1 mb-3 text-sm font-medium bg-white border border-solid rounded-2xl border-main text-main">海外股票型</div>
     
-    <!-- ETF -->
-    <EtfName :fund="fakeData" class="mb-5 lg:mb-8 text-2xl lg:text-3xl text-[#333]" />
-    
-    <!-- 最新狀態 -->
-    <EtfInfoBox :fund="fakeData" class="lg:w-2/3" />
+      <!-- ETF -->
+      <EtfName :fund="fakeData" class="mb-5 lg:mb-8 text-2xl lg:text-3xl text-[#333]" />
+      
+      <!-- 最新狀態 -->
+      <EtfInfoBox :fund="fakeData" class="lg:w-2/3" />
 
-    <!-- 內頁分頁下拉&tabs -->
-    <select v-model="currentTab" class="mb-5 selects lg:hidden" @change="handleTabPageChange(currentTab)">
-      <option v-for="tab in tabs" :value="tab.name" :key="tab.label">
-        {{ tab.label }}
-      </option>
-    </select>
-    
-    <div class="hidden grid-cols-6 gap-8 mb-16 lg:grid">
-      <RoundButton
-        v-for="tab in tabs"
-        :label="tab.label"
-        :key="tab.name" 
-        hover
-        class="!py-3 !text-lg !rounded-full"
-        :active="tab.name === currentTab"
-        @click="handleTabPageChange(tab.name)"
-      />
+      <!-- 內頁分頁下拉&tabs -->
+      <select v-model="currentTab" class="mb-5 selects lg:hidden" @change="handleTabPageChange(currentTab)">
+        <option v-for="tab in tabs" :value="tab.name" :key="tab.label">
+          {{ tab.label }}
+        </option>
+      </select>
+      
+      <div class="hidden grid-cols-6 gap-4 mb-16 lg:grid">
+        <RoundButton
+          v-for="tab in tabs"
+          :label="tab.label"
+          :key="tab.name" 
+          hover
+          class="!py-3 !text-lg !rounded-full"
+          :active="tab.name === currentTab"
+          @click="handleTabPageChange(tab.name)"
+        />
+      </div>
     </div>
 
-    <RouterView :fund="fakeData" />
+    <div class="bg-gradient -mt-[2px] lg:-mt-[90px]">
+      <div class="page-layout lg:pt-10">
+        <RouterView :fund="fakeData" />
+      </div>
+    </div>
     
     <div class="flex justify-center pt-6 pb-10">
       <RouterLink :to="{ name: ROUTE_NAME.etf.list.name }">
