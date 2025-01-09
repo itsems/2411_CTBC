d235f0d0825cd392686061ab097f409ea7b27f05
Emma <emma801006@hotmail.com>
2025-01-10 01:59:23 +0800

    feat: adjust navboard 0110

---
+++ src/components/NavBoardSlider.vue
@@ -0,0 +1,75 @@
+<template>
+  <div class="relative w-[820px] mx-auto navboard-slider">
+    <!-- :autoplay="{
+        delay: 12000,
+        disableOnInteraction: false
+      }" -->
+    <Swiper
+      class="!overflow-visible swiper-container"
+      :slidesPerView="1"
+      :loop="true"
+      :pagination="pagination"
+      :modules="modules"
+      :effect="'fade'"
+      :fadeEffect="{ crossFade: true }"
+    >
+      <SwiperSlide >
+        <img src="@/assets/images/navboard/banner902.jpg" class="block w-full" alt="00902" />
+      </SwiperSlide>
+      <SwiperSlide >
+        <img src="@/assets/images/navboard/banner902.jpg" class="block w-full" alt="00902" />
+      </SwiperSlide>
+      <SwiperSlide >
+        <img src="@/assets/images/navboard/banner902.jpg" class="block w-full" alt="00902" />
+      </SwiperSlide>
+      <SwiperSlide >
+        <img src="@/assets/images/navboard/banner902.jpg" class="block w-full" alt="00902" />
+      </SwiperSlide>
+      <SwiperSlide >
+        <img src="@/assets/images/navboard/banner902.jpg" class="block w-full" alt="00902" />
+      </SwiperSlide>
+      <!-- <div class="swiper-pagination"></div> -->
+    </Swiper>
+  </div>
+</template>
+<script setup>
+import { Swiper, SwiperSlide } from 'swiper/vue'
+import { Navigation, Pagination, EffectFade, Autoplay } from 'swiper/modules'
+
+// Swiper Configurations
+import 'swiper/css'
+import 'swiper/css/pagination'
+import 'swiper/css/effect-fade'
+
+const modules = [Navigation, Pagination, EffectFade, Autoplay]
+const pagination = ref({
+  clickable: true,
+  bulletActiveClass: 'active-white-bullet',
+  renderBullet: () => '<span class="swiper-pagination-bullet"><div class="inner-dot inner-white-dot"></div></span>'
+})
+</script>
+<style lang="scss">
+.navboard-slider {
+
+  .swiper-pagination-bullets.swiper-pagination-horizontal {
+    bottom: -37px
+  }
+  .swiper-pagination-bullet {
+    --swiper-pagination-bullet-size: 16px;
+    --swiper-pagination-bullet-horizontal-gap: 8px;
+    .inner-dot {
+     width: 16px;
+     height: 16px;
+     box-shadow: 2px 2px 4px 3px #ccc;
+   }
+  }
+  .swiper-pagination-bullet.active-white-bullet {
+    border: 1px solid transparent;
+    .inner-white-dot {
+     background-color: $mainGreen;
+   }
+  }
+
+}
+
+</style>
--- src/views/Home/NAVBoard.vue
+++ src/views/Home/NAVBoard.vue
@@ -1,16 +1,16 @@
 <template>
-  <div class="relative bg-gradient h-svh">
-    <div class="px-5 py-2 mb-16 bg-darkGreen">
-      <img class="w-[175px]" src="@/assets/images/logo-w.png" alt="中國信託投信 CTBC INVESTMENTS" />
+  <div class="relative bg-gradient w-[1050px] h-[1680px] mx-auto">
+    <div class="px-6 py-4 mb-9 bg-darkGreen">
+      <img class="w-[260px]" src="@/assets/images/logo-w.png" alt="中國信託投信 CTBC INVESTMENTS" />
     </div>
-    <div class="px-[10%]">
+    <div class="px-6 mb-8">
       
       <!-- Label Title -->
-      <div class="mb-16 text-5xl font-medium tracking-wider text-center text-main">法盛投資系列基金淨值</div>
+      <div class="text-6xl font-medium tracking-wider text-center mb-9 text-main">法盛投資系列基金淨值</div>
       
       <!-- Nav Title -->
-      <div class="grid shadow-md mb-4 text-center text-lg bg-main grid-cols-[1fr_200px_200px_200px_200px] text-white rounded-full py-2 mx-auto">
-        <span>基金名稱</span>
+      <div class="grid shadow-md mb-4 text-center text-4xl bg-main grid-cols-[1fr_100px_130px_155px_135px] text-white rounded-full py-4 mx-auto pr-4">
+        <span class="pl-16 text-left">基金名稱</span>
         <span>日期</span>
         <span>淨值</span>
         <span>漲跌</span>
@@ -18,59 +18,84 @@
       </div>
     
       <!-- List item Start -->
-      <div class="mb-4 list-item">
+      <div class="list-item">
         <span class="text-left">盧米斯賽勒斯債券基金-I/D美元級別</span>
         <span>1/6</span>
-        <span>11.9588</span>
+        <span>11.95</span>
         <span>
           <label class="font-medium" :class="isUpOrDownClass(0.1250)">
-            0.1250
             <Symbol :number="0.1250"  />
+            <span>0.1250</span>
           </label>
         </span>
-        <span :class="isUpOrDownClass(2.43)">2.43</span>
+        <span :class="isUpOrDownClass(2.43)">2.43%</span>
       </div>
       <!-- List item End -->
      
-      <div class="mb-4 list-item">
+      <div class="list-item">
         <span class="text-left">盧米斯賽勒斯非投資等級債券基金-I/D美元級別</span>
         <span>1/6</span>
         <span>4.70</span>
         <span>
           <label class="font-medium" :class="isUpOrDownClass(-0.01)">
-            -0.01
             <Symbol :number="-0.01"  />
+            <span>-0.01</span>
           </label>
         </span>
-        <span :class="isUpOrDownClass(-0.21)">-0.21</span>
+        <span :class="isUpOrDownClass(-0.21)">-0.21%</span>
       </div>
-      <div class="mb-4 list-item">
+      <div class="list-item">
         <span class="text-left">盧米斯賽勒斯全球機會債券基金-I/D美元級別</span>
         <span>1/6</span>
         <span>9.28</span>
         <span>
           <label class="font-medium" :class="isUpOrDownClass(-0.01)">
-            -0.01
             <Symbol :number="-0.01"  />
+            <span>-0.01</span>
           </label>
         </span>
-        <span :class="isUpOrDownClass(-0.21)">-0.21</span>
+        <span :class="isUpOrDownClass(-0.21)">-0.21%</span>
       </div>
-      <div class="mb-4 list-item">
+      <div class="list-item">
         <span class="text-left">盧米斯賽勒斯債券基金-R/DG美元級別</span>
         <span>1/6</span>
-        <span>11.9588</span>
+        <span>11.95</span>
         <span>
           <label class="font-medium" :class="isUpOrDownClass(0.1250)">
-            0.1250
             <Symbol :number="0.1250"  />
+            <span>0.1250</span>
           </label>
         </span>
-        <span :class="isUpOrDownClass(2.43)">2.43</span>
+        <span :class="isUpOrDownClass(2.43)">2.43%</span>
+      </div>
+      <div class="list-item">
+        <span class="text-left">盧米斯賽勒斯債券基金-R/DG美元級別</span>
+        <span>1/6</span>
+        <span>4.70</span>
+        <span>
+          <label class="font-medium" :class="isUpOrDownClass(0.1250)">
+            <Symbol :number="0.1250"  />
+            <span>0.1250</span>
+          </label>
+        </span>
+        <span :class="isUpOrDownClass(2.43)">2.43%</span>
+      </div>
+      <div class="list-item">
+        <span class="text-left">盧米斯賽勒斯債券基金-R/DG美元級別</span>
+        <span>1/6</span>
+        <span>11.95</span>
+        <span>
+          <label class="font-medium" :class="isUpOrDownClass(0.1250)">
+            <Symbol :number="0.1250"  />
+            <span>0.1250</span>
+          </label>
+        </span>
+        <span :class="isUpOrDownClass(2.43)">2.43%</span>
       </div>
     </div>
-    <div class="h-[1px] bg-[#d5d5d5] absolute w-full bottom-24" />
-    <p class="absolute text-[#333] text-lg -translate-x-1/2 bottom-10 left-1/2">以上為最新參考淨值，實際交易淨值以交易確認單為主，或請參考中國信託投信網站公告之淨值。</p>
+    <NavBoardSlider class="mb-16" />
+    <div class="h-[1px] bg-[#d5d5d5] w-full mb-4" />
+    <p class="text-[#333] text-2xl text-center">以上為最新參考淨值，實際交易淨值以交易確認單為主，或請參考中國信託投信網站公告之淨值。</p>
   </div>
 </template>
 <script setup>
@@ -78,6 +103,6 @@ import { isUpOrDownClass } from '@/utils/is-up-or-down-class'
 </script>
 <style scoped>
 .list-item {
-  @apply grid shadow-md pl-6 text-[#333] text-center text-2xl py-4 font-medium bg-white grid-cols-[1fr_200px_200px_200px_200px] rounded-full mx-auto;
+  @apply mb-4 grid shadow-md pl-6 text-[#333] leading-snug text-center text-4xl py-4 font-medium bg-white grid-cols-[1fr_100px_130px_155px_135px] rounded-[2rem] mx-auto pr-3;
 }
 </style>
