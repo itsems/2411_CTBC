f933fa1041772bbff22f143ab6b9f9f8e9e0f1f5
Emma <emma801006@hotmail.com>
2025-02-09 00:56:08 +0800

    chore: add fixed head of table
    ppt 0203 p.15

--- src/views/Etf/Rate.vue
+++ src/views/Etf/Rate.vue
@@ -27,42 +27,46 @@
       </div>
       <Hr />
       <div class="flex justify-between px-6 py-2">
-        <p class="text-grey-999">預收申購金額(%)</p>
-        <p>{{ fund.buyBackPrice }}</p>
-      </div>
-      <Hr />
-      <div class="flex justify-between px-6 pt-2">
-        <p class="text-grey-999">申購買回截止時間</p>
-        <p>{{ fund.time }}</p>
+        <p class="text-grey-999">日期</p>
+        <p>2025-02-07</p>
       </div>
     </div>
     
     <!-- Desktop -->
-    <div class="mobile-th-row-table text-md visible-desktop-block stripped-on-mobile-row">
-      <ul class="desktop-th lg:grid-cols-[2fr_1fr_1fr_1.5fr_1fr_1fr]">
-        <li class="!pl-4 text-left">ETF名稱</li>
-        <li>申購交易費率(%)</li>
-        <li>買回交易費率(%)</li>
-        <li class="min-w-fit">每基數申購買回手續費(元)</li>
-        <li>預收申購金額(%)</li>
-        <li>申購買回截止時間</li>
-      </ul>
-      <div v-for="fund in fakeData" :key="fund.code" class="data-row !grid-cols-[170px_1fr] lg:!grid-cols-[2fr_1fr_1fr_1.5fr_1fr_1fr]">
-        <div class="th">ETF名稱</div>
-        <div class="td lg:!pl-4">
-          <EtfName :fund="fund" />
+    <div class="mb-10 visible-desktop-block mobile-th-row-table stripped-on-mobile-row desktop-fixed-head">
+      <ul>
+        <li class="thead">
+          <div class="grid grid-cols-[2fr_1fr_1fr_1.5fr_150px] fixed-th">
+            <div class="flex justify-start pl-3">ETF名稱</div>
+            <div>申購交易費率(%)</div>
+            <div>買回交易費率(%)</div>
+            <div class="min-w-fit">每基數申購買回手續費(元)</div>
+            <div>日期</div>
+          </div>
+        </li>
+        <div v-for="fund in fakeData" :key="fund.code" class="data-row !grid-cols-[170px_1fr] lg:!grid-cols-[2fr_1fr_1fr_1.5fr_150px]">
+          <div class=" th">ETF名稱</div>
+          <div class="td lg:!pl-4">
+            <EtfName :fund="fund" />
+          </div>
+          <div class="th">申購交易費率(%)</div>
+          <div class="flex-center td">{{ fund.applyBuyRate }}</div>
+          <div class="th">買回交易費率(%)</div>
+          <div class="flex-center td">{{ fund.buyBackRate }}</div>
+          <div class="th">每基數申購買回手續費(元)</div>
+          <div class="flex-center td">{{ fund.fee }}</div>
+          <div class="th">日期</div>
+          <div class="flex-center td">2025-02-07</div>
         </div>
-        <div class="th">申購交易費率(%)</div>
-        <div class="flex-center td">{{ fund.applyBuyRate }}</div>
-        <div class="th">買回交易費率(%)</div>
-        <div class="flex-center td">{{ fund.buyBackRate }}</div>
-        <div class="th">每基數申購買回手續費(元)</div>
-        <div class="flex-center td">{{ fund.fee }}</div>
-        <div class="th">預收申購金額(%)</div>
-        <div class="flex-center td">{{ fund.buyBackPrice }}</div>
-        <div class="th">申購買回截止時間</div>
-        <div class="flex-center td">{{ fund.time }}</div>
-      </div>
+      </ul>
+    </div>
+    <p class="mt-10 mb-2">註：</p>
+    <div>
+      <ul class="mb-2 ml-5 text-sm list-decimal">
+        <li>上述申購/買回手續費，係包含集保事務處理費及券商事務處理費等。</li>
+        <li>各基金上市(櫃)日(含當日)後之申購/買回的交易費，會依信託契約或公開說明書的規定而有所變動。申購人/受益人如未依公開說明書規定繳足應付之實際申購總價金，將導致交易失敗者，經理公司得向申請人/受益人收取行政處理費。</li>
+        <li>債券ETF若採實際交易費率收取，投資人申贖當日之交易費率可能因證券市場現況(如:債券市場流動性及交易對手報價等)、投資組合交易部位(如:最佳化管理、現金管理、淨申贖之變動等)而與參考費率有差異，經理公司得依需要調整之。</li>
+      </ul>
     </div>
   </div>
 </template>
